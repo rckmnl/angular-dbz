8786f718706b607de60f978d2d09cd62662249e0
erick <rckmnlparedescabrera@gmail.com>
2023-06-29 12:13:31 -0400

    primer commit

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,15 @@
+import { CounterComponent } from './components/counter/counter.component';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+  exports: [
+    CounterComponent
+  ]
+
+
+})
+export class CounterModule {}
